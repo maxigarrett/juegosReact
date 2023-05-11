c467d51cff6462e2579f47ff8163ba5322a1ba19
maxigarrett <maxyg22@outlook.es>
2023-05-11 16:07:26 -0300

    apps de juegos letras por minutos terminada

--- src/components/WordsForMinutes.jsx
+++ src/components/WordsForMinutes.jsx
@@ -1,52 +1,99 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 const WORDS = [
+  "electroencefalografo",
+  "arteriosclerosis",
+  "idiosincrasia",
+  "transustanciacion",
+  "clavicembalo",
+  "paralelepipedo",
+  "delicuescencia",
+  "desoxirribonucleico",
   "abadas",
-  "mar",
+  "transustanciacion",
   "coliseo",
-  "jujuy",
+  "ovoviviparo",
   "karaoke",
   "arroba",
   "cangrejo",
   "manufactura",
   "prepago",
-  "caracortada",
+  "Hipopotomonstrosesquipedaliofobia",
   "telefono",
   "celular",
   "caracol",
   "uruguay",
   "argentina",
   "cojear",
-  "arremeter",
 ];
+
+const initialBuffer = {
+  buffer: "",
+};
 export const WordsForMinutes = () => {
   const [word, setWord] = useState(
     () => WORDS[(Math.random() * WORDS.length) | 0]
   );
   const [lettersCount, setLettersCounts] = useState(0);
-  const [buffer, setBuffer] = useState("");
+  const [buffer, setBuffer] = useState(initialBuffer);
+  const [time, setTime] = useState(0);
 
+  //PARA USARLO CON EL INPUT DE ENVIAR
   const handelSubmit = (event) => {
     event.preventDefault();
-    // console.log(event.target[0].value);
-    if (word === buffer) {
+    const { name, value } = event.target;
+    setBuffer({ ...buffer, [name]: value });
+    if (word === value) {
       setWord(WORDS[(Math.random() * WORDS.length) | 0]);
-      setLettersCounts((prevLetter) => prevLetter + buffer.length);
+      setLettersCounts((prevLetter) => prevLetter + value);
     }
     setBuffer("");
   };
 
+  //SE EJECUTA MIENTRAS TECLEAMOS
+  const handelSerch = (event) => {
+    const { name, value } = event.target;
+    setBuffer({ ...buffer, [name]: value });
+    console.log(name, value);
+    if (word === value) {
+      setWord(WORDS[(Math.random() * WORDS.length) | 0]);
+      setLettersCounts((prevLetter) => prevLetter + value.length);
+      setBuffer(initialBuffer);
+    }
+  };
+
+  //we start game for just 60 seconds
+  const startGame = () => {
+    setTime(60);
+  };
+
+  //we start to decrement the 60 sec to 0 sec fot finished the game
+  useEffect(() => {
+    if (time === 0) return;
+
+    setTimeout(() => {
+      setTime((preValu) => preValu - 1);
+    }, 1000);
+  }, [time]);
+
   return (
-    <>
+    <main>
       <h1>{word}</h1>
-      <h2>lettersForMinutes:{lettersCount}</h2>
-      <form onSubmit={handelSubmit}>
-        <input
-          type="text"
-          value={buffer}
-          onChange={(e) => setBuffer(e.target.value)}
-        />
-        <button type="submit">enviar</button>
-      </form>
-    </>
+      <span>Time: {time}</span>
+      <h2>letters quantity: {lettersCount}</h2>
+      {time === 0 ? (
+        <button onClick={startGame}>start</button>
+      ) : (
+        <form onSubmit={handelSubmit}>
+          <input
+            type="text"
+            name="buffer"
+            onChange={handelSerch}
+            value={buffer.buffer}
+            // onChange={(e) => setBuffer(e.target.value)}
+          />
+          <button type="submit">comprobar</button>
+        </form>
+      )}
+    </main>
   );
 };
